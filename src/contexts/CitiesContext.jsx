172e56bd195485791c706e2051e5ca8cdeefd3ec
bolaji87 <sakabolaji77@gmail.com>
2024-09-12 15:31:42 +0100

    feat: create Context api and use with custom hook

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,42 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:8000";
+const CitiesContext = createContext();
+
+const CitiesProvider = ({ children }) => {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchCities = async () => {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+};
+
+const useCities = () => {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error(
+      "CitiesContext was used outside of the CitiespPovider component"
+    );
+  return context;
+};
+export { CitiesProvider, useCities };
