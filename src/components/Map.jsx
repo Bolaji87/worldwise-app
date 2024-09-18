d0877a917dccf4c562ac69fb2175068fc1dec352
bolaji87 <sakabolaji77@gmail.com>
2024-09-18 15:26:35 +0100

    feat: add changes to component Map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,12 +11,18 @@ import {
 } from "react-leaflet";
 import { useCities } from "../contexts/CitiesContext";
 import { latLng } from "leaflet";
+import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "./Button";
+
 function Map() {
   const { cities } = useCities();
-
   const [mapPosition, setMapPosition] = useState([40, 0]);
-
   const [searchParams] = useSearchParams();
+  const {
+    isLoading: isLoadingPosition,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation();
 
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
@@ -25,8 +31,17 @@ function Map() {
     if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
   }, [mapLat, mapLng]);
 
+  useEffect(() => {
+    if (geolocationPosition)
+      setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+  }, [geolocationPosition]);
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use Your Position"}
+        </Button>
+      )}
       <MapContainer
         center={mapPosition}
         zoom={6}
