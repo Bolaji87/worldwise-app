8b3552d059bf743da391527977783d2544f89c91
Bolaji87 <sakabolaji77@gmail.com>
2025-03-11 00:30:14 +0100

    feat:update

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -7,6 +7,7 @@ import { useCities } from "../contexts/CitiesContext";
 
 function CountryList() {
   const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
@@ -19,6 +20,7 @@ function CountryList() {
       return [...arr, { country: city.country, emoji: city.emoji }];
     else return arr;
   }, []);
+
   return (
     <ul className={styles.countryList}>
       {countries.map((country) => (
